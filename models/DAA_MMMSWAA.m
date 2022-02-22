503fced322d2b6ecca06a325e5cdf1bd2e34dbba
Anders Stevnhoved Olsen <ansol@linuxterm1.imm.dtu.dk>
2022-02-22 11:00:27 +0100

    removed gpu compatibility and bsxfun

--- models/DAA_MMMSWAA.m
+++ models/DAA_MMMSWAA.m
@@ -11,42 +11,32 @@ function d=DAA_MMMSWAA(X,K,I,U,varargin)
     d.K = K;
 
     % Determine input dimensions
-    d.M = length(d.X); % number of modalities
+    d.M = length(d.X);  % number of modalities
     d.D = zeros(1,d.M); % number of dimensions in each modalities
-    for m = 1:d.M % for each modality, determine D
+    for m = 1:d.M       % for each modality, determine D
         [d.D(m), d.N, d.P, d.L] = size(d.X{m});
-        if d.gpu, d.X{m} = gpuArray(d.X{m}); end
     end
-
-    if nargin<4; U=logical(ones(d.N,1)); end
-    if nargin<3; I=logical(ones(d.N,1)); end
-
-    if d.gpu; d.U = gpuArray(U); d.I = gpuArray(I);
-    else; d.U = U; d.I = I; end
+    
+    % I is the time points after 0
+    if nargin<4; d.U=logical(ones(d.N,1)); else d.U = U; end
+    if nargin<3; d.I=logical(ones(d.N,1)); else d.I = I; end
 
     % Set number of iterations of C and S updates before switching
     d.niter= [1,1];
     %d.niter = [3,10];
     
-    if d.gpu; d.increase = gpuArray(1.1);
-    else; d.increase = 1.1; end
+    d.increase = 1.1;
+    d.decrease = 0.5;
 
-    if d.gpu; d.decrease = gpuArray(0.9);
-    else; d.decrease = 0.5; end
-
-    if d.gpu; d.muC = gpuArray(1.0);
-    else; d.muC = 1; end
-
-    if d.gpu; d.muS = 1e0*ones(1, d.N, d.P, d.M, d.L, d.precision, 'gpuArray');
-    else; d.muS = 1e0*ones(1, d.N, d.P, d.M, d.L); end
-
-    d.muC_history = [d.muC];
-    d.muS_history = [median(d.muS(:))];
+    %step sizes?
+    d.muC = 1;
+    d.muS = 1e0*ones(1, d.N, d.P, d.M, d.L);
+    d.muC_history = d.muC;
+    d.muS_history = median(d.muS(:));
 
     % Initilize C
     candidates = datasample(1:sum(d.I), K);
-    if d.gpu; d.C = zeros(sum(d.I), d.K,  d.precision, 'gpuArray');
-    else; d.C = zeros(sum(d.I), d.K); end
+    d.C = zeros(sum(d.I), d.K);
     for i=1:length(candidates)
        d.C(candidates(i), i) = 1;
     end
@@ -56,15 +46,10 @@ function d=DAA_MMMSWAA(X,K,I,U,varargin)
     d.dd = cell(1,d.M);
     d.Xf = cell(1,d.M);
     for m = 1:d.M
-        if d.gpu
-            [~, d.dd{m}] = pca(gather(mean(d.X{m},3)), 'NumComponents',1);
-            d.dd{m} = gpuArray(d.dd{m});
-        else
-            [~, d.dd{m}] = pca(mean(d.X{m},[3,4]), 'NumComponents',1);
-        end
-        projections = sum(bsxfun(@times, d.X{m}, d.dd{m}), 1);
+        [~, d.dd{m}] = pca(mean(d.X{m},[3,4]), 'NumComponents',1);
+        projections = sum(d.X{m}.*d.dd{m}, 1);
         to_flip = 2*(projections < 0)-1;
-        d.Xf{m} = bsxfun(@times, d.X{m}, to_flip);
+        d.Xf{m} = d.X{m}.* to_flip;
         d.dd{m} = normc(d.dd{m});
     end
 
@@ -88,18 +73,16 @@ function d=DAA_MMMSWAA(X,K,I,U,varargin)
     % Initialize S
 
     dim = [d.K, sum(d.U), d.P, d.M, d.L];
-    if d.gpu; d.S = -log(rand(dim, d.precision, 'gpuArray'));
-    else; d.S = -log(rand(dim)); end
+    d.S = -log(rand(dim));
 
     for m = 1:d.M
-        d.S(:,:,:,m) = bsxfun(@rdivide, d.S(:,:,:,m), sum(d.S(:,:,:,m),1));
+        d.S(:,:,:,m) = d.S(:,:,:,m)./sum(d.S(:,:,:,m),1);
     end
 
     % Determine initial reconstruction
     d.XCS = cell(d.M,1);
     for m = 1:d.M
-        if d.gpu; d.XCS{m} = nan(d.D(m), d.N, d.P, d.L, d.precision, 'gpuArray');
-        else;  d.XCS{m} = nan(d.D(m), d.N, d.P, d.L); end
+        d.XCS{m} = nan(d.D(m), d.N, d.P, d.L);
         for l = 1:d.L
             for p = 1:d.P
                 d.XCS{m}(:, :, p, l) = d.XC{m}(:, :, p, l) * d.S(:, :, p, m, l);
@@ -109,8 +92,7 @@ function d=DAA_MMMSWAA(X,K,I,U,varargin)
 
     % Determine initial loss
 
-    if d.gpu; d.loss = nan(1, d.N, d.P, d.M, d.L, d.precision, 'gpuArray');
-    else;  d.loss = nan(1, d.N, d.P, d.M, d.L); end
+    d.loss = nan(1, d.N, d.P, d.M, d.L);
 
     for m = 1:d.M
         for l = 1:d.L
@@ -187,12 +169,6 @@ function d=DAA_MMMSWAA(X,K,I,U,varargin)
            end
        end
    end
-   if d.gpu
-        fields = fieldnames(d);
-        for i = 1:numel(fields)
-            d.(fields{i}) = gather(d.(fields{i}));
-        end
-   end
 
 function var = mgetopt(opts, varname, default, varargin)
     % Parser for optional arguments
@@ -236,18 +212,18 @@ function d = Supdate(d)
         z   = sum(x.*xcs, 1);
         v = (1./q).*z;
 
-        tv = bsxfun(@times, x-bsxfun(@times, xcs, v), v);
+        tv = (x-xcs.*v).*v;
 
         loss_old = loss;
 
         g = -2*xc'*tv;
-        g = bsxfun(@minus, g, sum(bsxfun(@times, g, s)));
+        g = g - sum(g.*s);
 
         s_old = s;
 
-        s = s_old-bsxfun(@times,g,mus);
+        s = s_old-g.*mus;
         s(s<0) = 0;
-        s = bsxfun(@rdivide,s,sum(s,1));
+        s = s./sum(s,1);
 
         xcs = xc * s;
         q   = sum(xcs.^2, 1);
@@ -257,17 +233,12 @@ function d = Supdate(d)
         loss = -v.^2;
 
         idx = loss <= loss_old;
-
-        if d.gpu
-            mus = mus.*idx.*d.increase+mus.*(~idx).*d.decrease;
-            s = bsxfun(@times, s, idx)+bsxfun(@times, s_old, ~idx);
-            loss = bsxfun(@times, l, idx)+bsxfun(@times, loss_old, ~idx);
-        else
-            mus(idx) = mus(idx)*d.increase;
-            mus(~idx) = mus(~idx)*d.decrease;
-            s(:, ~idx) = s_old(:, ~idx);
-            loss(~idx) = loss_old(~idx);
-        end
+        
+        mus(idx) = mus(idx)*d.increase;
+        mus(~idx) = mus(~idx)*d.decrease;
+        s(:, ~idx) = s_old(:, ~idx);
+        loss(~idx) = loss_old(~idx);
+        
 
         d.S(:, :, p, m, l) = s;
         d.muS(:,:, p, m, l) = mus;
@@ -281,8 +252,7 @@ function d=Cupdate(d)
     for k=1:d.niter(1)
         loss_old=d.loss;
 
-        if d.gpu; G = zeros(sum(d.I), d.K, d.P, d.M, d.L,  d.precision, 'gpuArray');
-        else;  G = zeros(sum(d.I), d.K, d.P, d.M, d.L); end
+        G = zeros(sum(d.I), d.K, d.P, d.M, d.L);
 
         for m = 1:d.M
             d.XC{m} = permute(reshape(d.C'*d.Xfs{m}, [d.K, d.D(m), d.P, d.L]), [2,1,3,4]);
@@ -299,10 +269,10 @@ function d=Cupdate(d)
                     z   = sum(x.*xcs, 1);
                     v = (1./q).*z;
 
-                    tv = bsxfun(@times, x-bsxfun(@times, xcs, v), v);
+                    tv = (x-xcs.*v).*v;
 
                     g = -2*xf'*(tv*s');
-                    g = bsxfun(@minus,g,sum(bsxfun(@times,g,d.C)));
+                    g = g-sum(g.*d.C);
                     G(:,:,p,m,l) = g;
                 end
             end
@@ -312,9 +282,9 @@ function d=Cupdate(d)
         stop = 0;
         C_old = d.C;
         while ~stop
-            d.C = C_old-bsxfun(@times, g, d.muC);
+            d.C = C_old-g.*d.muC;
             d.C(d.C < 0)=0;
-            d.C = bsxfun(@rdivide, d.C, sum(d.C) + eps);
+            d.C = d.C./(sum(d.C) + eps);
             for m=1:d.M
                 d.XC{m} = permute(reshape(d.C'*d.Xfs{m},...
                                     [d.K, d.D(m), d.P, d.L]), [2,1,3,4]);
